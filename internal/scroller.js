89d89a4ae2caf53812f12d56d0942afce7ee2dfb
david siah <dsiah@villanova.edu>
2016-02-02 12:42:53 -0500

    preliminary link hopper

---
+++ internal/scroller.js
@@ -0,0 +1,19 @@
+var tableau = {
+	'am-hop': 'am-block',
+	'r-hop': 'r-block',
+	'c-hop': 'c-block',
+	't-hop': 't-block',
+};
+
+function scrollById() {
+	var dest = tableau[this.id];
+	document.getElementById(dest).scrollIntoView({
+		block: "end", behavior: "smooth"
+	});
+}
+
+var hoplinks = document.getElementsByClassName('toc-item');
+
+for (var ind = 0; ind < hoplinks.length; ind++) {
+	hoplinks[ind].onclick = scrollById;
+}
